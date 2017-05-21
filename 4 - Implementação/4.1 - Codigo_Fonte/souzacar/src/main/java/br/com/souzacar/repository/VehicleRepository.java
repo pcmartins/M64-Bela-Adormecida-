719e888048a008cad9d5126595d468871e420fee
DESKTOP-657R2DA\super <pcmartins.santos@gmail.com>
2017-05-20 22:51:14 -0300

    Adequação back-end

--- 4 - Implementação/4.1 - Codigo_Fonte/souzacar/src/main/java/br/com/souzacar/repository/VehicleRepository.java
+++ 4 - Implementação/4.1 - Codigo_Fonte/souzacar/src/main/java/br/com/souzacar/repository/VehicleRepository.java
@@ -1,9 +1,9 @@
-package br.com.souzacar.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.souzacar.model.Vehicle;
-
-public interface VehicleRepository extends CrudRepository<Vehicle, Long> {
-
-}
+//package br.com.souzacar.repository;
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//import br.com.souzacar.model.Vehicle;
+//
+//public interface VehicleRepository extends CrudRepository<Vehicle, Long> {
+//
+//}
